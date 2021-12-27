763b6f6b6a7ad01a6a5c57d4868004a56f5f974a
Kacy Lombard <klombard6@gatech.edu>
2021-12-27 14:40:22 -0500

    Finalizes docker containerization of database, backend, and handles database migrations

---
+++ tutorcare-core/db/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,2 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS users(user_id uuid DEFAULT uuid_generate_v4() NOT NULL, first_name varChar(255), last_name varChar(255), email varChar(255), password varChar(255), date_joined TIMESTAMPTZ, status boolean, PRIMARY KEY (user_id));
