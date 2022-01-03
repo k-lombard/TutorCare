a698422a51500a0c60af74469bcde1186c73883e
hacker-wipr <christopher.ballenger.wipr@statefarm.com>
2022-01-03 12:59:10 -0500

    UI Work on Login and Sign Up Pages. Added cards around the forms, changed filled text fields to outlines, and other changes.

--- tutorcare-core/client/src/app/app.module.ts
+++ tutorcare-core/client/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
 import { UsersService } from './users.service';
 import { Http, ConnectionBackend, HttpModule } from '@angular/http';
 import { MatButtonModule } from '@angular/material/button';
@@ -38,6 +40,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
+    MatCardModule,
     HttpClientModule,
     MatButtonModule,
     ReactiveFormsModule,
