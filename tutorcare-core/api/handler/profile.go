52846eec01fc1b3b797edf6b83b6c60d858d75a5
zaneHawkins <70725038+zaneHawkins@users.noreply.github.com>
2022-03-02 10:30:09 -0500

    Create Endpoints for Profile Page

--- tutorcare-core/api/handler/profile.go
+++ tutorcare-core/api/handler/profile.go
@@ -12,13 +12,35 @@ import (
 )
 
 func (r routes) profile(rg *gin.RouterGroup) {
-	rg.PUT("/:userid", updateUserProfile)
+	rg.PUT("/:userid", updateUserData)
+	rg.PUT("/p/:userid", updateUserProfile)
+	rg.GET("/p/:userid", getUserProfileById)
+}
+
+func updateUserData(c *gin.Context) {
+	r := c.Request
+	userID := uuid.MustParse(c.Param("userid"))
+	userData := models.User{}
+	if err := render.Bind(r, &userData); err != nil {
+		c.JSON(http.StatusBadRequest, "Error: Bad request")
+		return
+	}
+	user, err := dbInstance.UpdateUserData(userID, userData)
+	if err != nil {
+		if err == database.ErrNoMatch {
+			c.JSON(http.StatusNotFound, "Error: Resource not found")
+		} else {
+			c.JSON(http.StatusInternalServerError, "Internal Server Error")
+		}
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
 
 func updateUserProfile(c *gin.Context) {
 	r := c.Request
 	userID := uuid.MustParse(c.Param("userid"))
-	userData := models.User{}
+	userData := models.Profile{}
 	if err := render.Bind(r, &userData); err != nil {
 		c.JSON(http.StatusBadRequest, "Error: Bad request")
 		return
@@ -33,4 +55,18 @@ func updateUserProfile(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, user)
+}
+
+func getUserProfileById(c *gin.Context) {
+	userID := uuid.MustParse(c.Param("userid"))
+	userProfile, err := dbInstance.GetUserProfileById(userID)
+	if err != nil {
+		if err == database.ErrNoMatch {
+			c.JSON(http.StatusNotFound, "Error: Resource not found")
+		} else {
+			c.JSON(http.StatusBadRequest, "Error: Bad request")
+		}
+		return
+	}
+	c.JSON(http.StatusOK, userProfile)
 }
